0ee90d0c79b70c07b1d3aaaa9d63c5bb0045a7f1
hectorg2211 <hector032716@gmail.com>
2021-11-18 11:16:58 -0800

    Added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__divider">
+        <div className="left"></div>
+        <div className="right"></div>
+      </div>
+      <div className="footer__info">
+        <div className="footer__info-column">
+          <h3 className="h3 h3--2">Our products</h3>
+          <p className="p p--2"></p>
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <p className="p p--2">Copyright &copy; 2021 PML HOLIDAYS</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
