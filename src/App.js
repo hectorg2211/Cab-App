a0628d599c0aa5ab99b09d66d1f1fff48f54d15b
hectorg2211 <hector032716@gmail.com>
2021-11-17 21:51:26 -0800

    Added benefits section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./scss/App.scss";
 // Components
 import Header from "./components/Header";
 import BookingSection from "./components/BookingSection";
+import Benefits from "./components/Benefits";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <BookingSection />
+      <Benefits />
     </div>
   );
 }
