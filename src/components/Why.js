3bb05f733a21ebe74f08ad2e46b6d6aaaaab1fdc
hectorg2211 <hector032716@gmail.com>
2021-11-28 11:11:34 -0800

    Added why section icons

--- src/components/Why.js
+++ src/components/Why.js
@@ -1,4 +1,8 @@
 import React from "react";
+import LocalTaxiRoundedIcon from "@mui/icons-material/LocalTaxiRounded";
+import PriceCheckRoundedIcon from "@mui/icons-material/PriceCheckRounded";
+import AltRouteRoundedIcon from "@mui/icons-material/AltRouteRounded";
+import EventRoundedIcon from "@mui/icons-material/EventRounded";
 
 const Why = () => {
   return (
@@ -10,38 +14,30 @@ const Why = () => {
       </div>
       <div className="why__container">
         <div className="why__item">
-          <div className="img">
-            <img src="./assets/car.png" alt="car logo" />
-          </div>
+          <LocalTaxiRoundedIcon />
           <h3 className="h3 h3--1">Extensive Options</h3>
           <p className="p p--1">
             Wide range of quality, safe & licensed vehicles
           </p>
         </div>
         <div className="why__item">
-          <div className="img">
-            <img src="./assets/hand.png" alt="car logo" />
-          </div>
-          <h3 className="h3 h-3-1">Convenient</h3>
+          <PriceCheckRoundedIcon />
+          <h3 className="h3 h3--1">Convenient</h3>
           <p className="p p--1">
             Enjoy a high-quality transfer experience at surprisingly low prices
           </p>
         </div>
         <div className="why__item">
-          <div className="img">
-            <img src="./assets/turn.png" alt="car logo" />
-          </div>
-          <h3 className="h3 h-3-1">Easy & flexible booking</h3>
+          <AltRouteRoundedIcon />
+          <h3 className="h3 h3--1">Easy & flexible booking</h3>
           <p className="p p--1">
             Booking online is easy and only take 5 minutes, cancellations are
             free of charge up to 24 hours before the transfer.
           </p>
         </div>
         <div className="why__item">
-          <div className="img">
-            <img src="./assets/availability.png" alt="car logo" />
-          </div>
-          <h3 className="h3 h-3-1">24 / 7 customer service</h3>
+          <EventRoundedIcon />
+          <h3 className="h3 h3--1">24 / 7 customer service</h3>
           <p className="p p--1">
             Our office is staffed 24 hours a day, 365 days a year- we're always
             here to help you.
