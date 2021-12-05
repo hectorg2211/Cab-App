2201d403498f69752da4b01263ec89419d551cd5
hectorg2211 <hector032716@gmail.com>
2021-12-05 09:36:04 -0800

    Added dashboard navbar

--- src/components/DashboardScreen.js
+++ src/components/DashboardScreen.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DashboardScreen = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashboard">
+      <div className="navbar">
+        <div className="navbar__info">
+          <div className="navbar__image">
+            <img
+              src="https://images.unsplash.com/photo-1604004555489-723a93d6ce74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+              alt="user"
+            />
+          </div>
+          <h3 className="h3 h3--2">User name</h3>
+        </div>
+
+        <div className="navbar__container">
+          <Link to="/cabs">
+            <h3 className="h3 h3--2">Cabs</h3>
+          </Link>
+          <Link to="/cabs">
+            <h3 className="h3 h3--2">Reports</h3>
+          </Link>
+        </div>
+      </div>
+      <div className="data">Hello</div>
+    </div>
+  );
 };
 
 export default DashboardScreen;
